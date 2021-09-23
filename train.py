dbf1b71af8fb9a091684336a8af5ff55e8894b39
mbordyuh <mykola.bordyuh@gmail.com>
2021-09-23 17:17:47 -0400

    create images directory

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from util import sample_2d_data
 import torch
 from model import mlp, score_matching
 from vis_utils import plot_gradients, make_a_gif
-
+import os
 device = 'cpu'
 dataset = sample_2d_data(dataset='8gaussians', n_samples=10000)
 model = mlp(sizes=[2, 128, 128, 2])
@@ -10,6 +10,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 def train_and_visualize():
+
     for epoch in range(100):
         loss = score_matching(model, dataset)
         optimizer.zero_grad()
@@ -21,5 +22,9 @@ def train_and_visualize():
 
 
 if __name__ == '__main__':
+
+    if not os.path.exists('images'):
+        os.makedirs('images')
+
     train_and_visualize()
     make_a_gif(images=f'images/*.png', path='gradient.gif')
